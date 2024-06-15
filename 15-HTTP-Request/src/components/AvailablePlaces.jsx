10370f868959caeb539c73d2c9fc7a0969a6ec22
Rush-2K <syaidiazhari@gmail.com>
2024-06-16 02:09:51 +0800

    send data using POST req

--- 15-HTTP-Request/src/components/AvailablePlaces.jsx
+++ 15-HTTP-Request/src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -14,23 +15,18 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true)
 
       try{
-        const response = await fetch('http://localhost:3000/places')
-        const resData = await response.json();
-
-        if(!response.ok){
-          throw new Error('Failed to fetch places');
-        } //200, 300 success, 400, 500 error
+        const places = await fetchAvailablePlaces();
 
         navigator.geolocation.getCurrentPosition((position) => {
           const sortedPlaces = sortPlacesByDistance(
-            resData.places, 
+            places, 
             position.coords.latitude, 
             position.coords.longitude
           );
           setAvailablePlaces(sortedPlaces)
         });
 
-        setAvailablePlaces(resData.places)
+        setAvailablePlaces(places)
         setIsFetching(false)
       }catch(error){
         setError({message: error.message || 'Could not fetch places, please try again later'});
