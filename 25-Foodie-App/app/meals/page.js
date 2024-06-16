6d4e758af945f90aeb6a0d87204110f412acf6e2
Rush-2K <syaidiazhari@gmail.com>
2024-06-16 19:27:12 +0800

    commit

--- 25-Foodie-App/app/meals/page.js
+++ 25-Foodie-App/app/meals/page.js
@@ -1,4 +1,25 @@
+import Link from 'next/link'
+import classes from './page.module.css'
+
 export default function MealsPage() {
-    return <h1>Meals Page</h1>;
+    return (
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicous Meal, created
+                    <span className={classes.highlight}>by you</span>
+                </h1>
+                <p>Choose your favorite recipes and cook it yourself</p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">
+                        Share your favorite recipes
+                    </Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                
+            </main>
+        </>
+    )
 }
 
