0eb80ef7aa4dd5777aaa58eb78ce4c0347705799
Rush-2K <syaidiazhari@gmail.com>
2024-08-06 22:27:44 +0800

    use action creator thunk

--- 20-Refresher-Practice-Part1/src/App.js
+++ 20-Refresher-Practice-Part1/src/App.js
@@ -5,7 +5,8 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { Fragment, useEffect } from 'react';
 import { uiActions } from './store/ui-slice';
-import Notification from './components/UI/Notification'
+import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -17,43 +18,44 @@ function App() {
   const notification = useSelector(state => state.ui.notification)
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!'
-      }))
-      const response = await fetch(
-        'https://react-http-5e77e-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart)
-        }
-      )
-      if (!response.ok){
-        // throw new Error('Sending cart data failed')
-        
-      }
+    // const sendCartData = async () => {
+      // dispatch(uiActions.showNotification({
+      //   status: 'pending',
+      //   title: 'Sending...',
+      //   message: 'Sending cart data!'
+      // }))
+      // const response = await fetch(
+      //   'https://react-http-5e77e-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
+      //   {
+      //     method: 'PUT',
+      //     body: JSON.stringify(cart)
+      //   }
+      // )
+      // if (!response.ok){
+      //   throw new Error('Sending cart data failed')
+      // }
       
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Success',
-        message: 'Sent cart data successfully!'
-      }))
-    } 
+      // dispatch(uiActions.showNotification({
+      //   status: 'success',
+      //   title: 'Success',
+      //   message: 'Sent cart data successfully!'
+      // }))
+    // } 
 
     if(isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error!',
-        message: 'Sent cart data failed'
-      }))
-    })
+    dispatch(sendCartData(cart));
+
+    // sendCartData().catch((error) => {
+    //   dispatch(uiActions.showNotification({
+    //     status: 'error',
+    //     title: 'Error!',
+    //     message: 'Sent cart data failed'
+    //   }))
+    // })
   }, [cart, dispatch])
   
   return (
