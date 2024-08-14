3018292f27e17bb18e7dbf0ec6cd1031a160eeef
Rush-2K <syaidiazhari@gmail.com>
2024-08-14 22:40:46 +0800

    manage token expiration

--- 22-Authentication/frontend/util/auth.js
+++ 22-Authentication/frontend/util/auth.js
@@ -1,6 +1,25 @@
+import {redirect} from 'react-router-dom';
+
+export function getTokenDuration() {
+    const storedExpirationDate = localStorage.getItem('expiration'); //first store the expiration date
+    const expirationDate = new Date(storedExpirationDate); //transform it to date object
+    const now = new Date(); //get the current date/timestamp
+    const duration = expirationDate.getTime() - now.getTime(); //get the remaining duration in miliseconds .getTime()
+    return duration;
+}
+
 export function getAuthToken() {
     const token = localStorage.getItem('token');
-    return token;
+
+    if(!token) {
+        return null; //should be null not undefined, bcs the loader wont return that value to the components
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    if(tokenDuration < 0) { //the token is expired, no remaining time
+        return 'EXPIRED';
+    };
 }
 
 export function tokenLoader() {
