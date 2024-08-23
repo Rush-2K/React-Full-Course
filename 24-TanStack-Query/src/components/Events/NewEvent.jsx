3420a16ef7069c4ab0e2e6c81453cec3c34357c9
Rush-2K <syaidiazhari@gmail.com>
2024-08-23 21:39:19 +0800

    added mutation success and invalidating queries

--- 24-TanStack-Query/src/components/Events/NewEvent.jsx
+++ 24-TanStack-Query/src/components/Events/NewEvent.jsx
@@ -5,12 +5,19 @@ import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import { createNewEvent } from '../../util/https.js';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
+import { queryClient } from '../../util/https.js';
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
   const { mutate, isPending, isError, error } = useMutation({
-    mutationFn: createNewEvent
+    mutationFn: createNewEvent,
+    onSuccess: () => {      //will be executed if the mutation is succeeed
+      queryClient.invalidateQueries({
+        queryKey: ['events']
+      });    //tells react that data fetched by certain query is outdated(based on the querykey, sort of like telling them). so it should be refetched/updated.
+      navigate('/events');
+    }
   });
 
   function handleSubmit(formData) {
