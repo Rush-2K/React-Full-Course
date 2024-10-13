c50944d71839bf23227df48154fe548cf2eb4d83
Rush-2K <syaidiazhari@gmail.com>
2024-10-13 20:40:12 +0800

    add 404/not found page

--- 25-Foodie-App/components/lib/meals.js
+++ 25-Foodie-App/components/lib/meals.js
@@ -11,7 +11,7 @@ const db = new sqlite3.Database('meals.db', (err) => {
 export async function getMeals() {
   // Simulate a delay
   await new Promise((resolve) => setTimeout(resolve, 4000));
-  throw new Error('Loading meals failed');
+  // throw new Error('Loading meals failed');
   // Use a Promise to handle the database query asynchronously
   return new Promise((resolve, reject) => {
     db.all('SELECT * FROM meals', [], (err, rows) => {
