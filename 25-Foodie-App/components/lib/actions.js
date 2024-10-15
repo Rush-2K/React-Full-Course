19a42c05165c96e922a7b81cc6c0d2692521be88
Rush-2K <syaidiazhari@gmail.com>
2024-10-15 21:30:51 +0800

    add server side validation

--- 25-Foodie-App/components/lib/actions.js
+++ 25-Foodie-App/components/lib/actions.js
@@ -1,7 +1,11 @@
 'use server';
 
 import { redirect } from 'next/navigation';
-import { saveMeal } from './meals'
+import { saveMeal } from './meals';
+
+function isInvalidText(text) {
+  return !text || text.trim() === '';
+}
 
 export async function shareMeal(formData) {
     const meal = {
@@ -11,6 +15,18 @@ export async function shareMeal(formData) {
       image: formData.get('image'),
       creator: formData.get('name'),
       creator_email: formData.get('email')
+    }
+
+    if(
+      !isInvalidText(meal.title) || 
+      isInvalidText(meal.summary) || 
+      isInvalidText(meal.instructions) || 
+      isInvalidText(meal.creator) || 
+      isInvalidText(meal.creator_email) || 
+      !meal.creator_email.includes('@') ||
+      !meal.image || meal.image.size === 0
+    ){
+      throw new Error('Invalid Input')
     }
 
     await saveMeal(meal);
