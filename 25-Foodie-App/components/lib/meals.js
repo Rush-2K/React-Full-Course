60f5bb0ea6a340924b5c2cbe930ed07704d9f367
Rush-2K <syaidiazhari@gmail.com>
2024-10-15 21:10:05 +0800

    store uploaded images using node js api file system and store data in database

--- 25-Foodie-App/components/lib/meals.js
+++ 25-Foodie-App/components/lib/meals.js
@@ -1,3 +1,5 @@
+import fs from 'node:fs';  //node js file api system
+
 import sql from 'better-sqlite3';
 import slugify from 'slugify';
 import xss from 'xss';
@@ -14,7 +16,35 @@ export function getMeal(slug) {
     return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
 }
 
-export function saveMeal(meal) {
+export async function saveMeal(meal) {
     meal.slug = slugify(meal.title, { lower: true});
     meal.instructions = xss(meal.instructions);
+
+    const extension = meal.image.name.split('.').pop(); //get the extension e.g. .jpg/.png
+    const fileName = `${meal.slug}.${extension}`
+
+    const stream = fs.createWriteStream(`public/images/${fileName}`) // allow us to create a stream to write a certain data to a certain file
+    const bufferedImage = await meal.image.arrayBuffer();
+
+    stream.write(Buffer.from(bufferedImage), (error) => {
+        if(error){
+            throw new Error('Saving image failed!');
+        }
+    });
+
+    meal.image = `/images/${fileName}` //save only the filename in database 
+
+    db.prepare(`
+        INSERT INTO meals
+        (title, summary, instructions, creator, creator_email, image, slug)
+        VALUES (
+         @title,
+         @summary,
+         @instructions,
+         @creator,
+         @creator_email,
+         @image,
+         @slug
+        )
+    `).run(meal);
 }
