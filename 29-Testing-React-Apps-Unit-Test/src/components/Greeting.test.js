69a0cbebdd337c617788717dc37b5ebc9a2f302a
Rush-2K <syaidiazhari@gmail.com>
2024-10-22 20:09:09 +0800

    first test

---
+++ 29-Testing-React-Apps-Unit-Test/src/components/Greeting.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greeting";
+
+test('renders Hellow World as a text', () => {
+    //Arrange
+    render(<Greeting />) //render the component
+
+    //Act
+    //.. nothing
+
+    //Asset
+    const helloWorldElement = screen.getByText('Hello World', { exact: false });
+    expect(helloWorldElement).toBeInTheDocument();
+});
