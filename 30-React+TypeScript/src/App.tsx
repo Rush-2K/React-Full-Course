8a3fa107c7f791b20685582f14c9db703f667cb8
Rush-2K <syaidiazhari@gmail.com>
2024-10-23 12:49:24 +0800

    add data model

--- 30-React+TypeScript/src/App.tsx
+++ 30-React+TypeScript/src/App.tsx
@@ -1,9 +1,15 @@
 import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn React 123')
+  ]
+
   return (
     <div className="App">
-      <Todos items={['Learn React', 'Learn TypeScripttt']}/>
+      <Todos items={todos}/>
     </div>
   );
 }
